0ec8ef7e5faf858740dc7dcde3df793456ed6c19
Advaita <vedanta3000@gmail.com>
2024-03-21 11:01:48 +0530

    updated model

--- models/SubTask.js
+++ models/SubTask.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const subTaskSchema = new mongoose.Schema({
-  id: { type: Number, unique: true },
   task_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Task', required: true },
   status: { type: Number, enum: [0, 1], default: 0 },
   created_at: { type: Date, default: Date.now },
